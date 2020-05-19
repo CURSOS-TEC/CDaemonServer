346d8e60ac58aa9397a9cd37646ca30c4f5cf60f
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2020-05-19 15:30:50 -0600

    add server

--- daemon/server/CEServer.c
+++ daemon/server/CEServer.c
@@ -14,11 +14,11 @@ int start_server(CEServerStr serverStr)
 {
   char server_messages[1024] = "HTTP/1.1 200 OK\r\n"
 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
-"<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
-"<style>body { background-color: #111 }"
-"h1 { font-size:4cm; text-align: center; color: black;"
+"<!DOCTYPE html><html><head><title>CE IMAGE Server</title>"
+"<style>body { background-color: #FFFFF }"
+"h1 { font-size:2cm; text-align: center; color: black;"
 " text-shadow: 0 0 2mm red}</style></head>"
-"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+"<body><h1> CE IMMAGE SERVER</h1></body></html>\r\n";
   // create the server socket
   int server_socket;
 
